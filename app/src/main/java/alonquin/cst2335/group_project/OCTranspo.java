62553a2de960026a4ad5e3636007ac81ad552a51
Caryourday <adetkay@gmail.com>
2018-03-04 14:14:55 -0500

    Initial commit from Adetunji Kayode

---
+++ app/src/main/java/alonquin/cst2335/group_project/OCTranspo.java
@@ -0,0 +1,13 @@
+package alonquin.cst2335.group_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OCTranspo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_octranspo);
+    }
+}
