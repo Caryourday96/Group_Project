42548c1e6bc452e52002253dce987cd62be12daa
Ben <gosseb@live.ca>
2018-03-11 18:25:05 -0400

    initial commit

--- app/src/main/java/alonquin/cst2335/group_project/OCTranspo.java
+++ app/src/main/java/alonquin/cst2335/group_project/OCTranspo.java
@@ -2,12 +2,46 @@ package alonquin.cst2335.group_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class OCTranspo extends AppCompatActivity {
+
+    protected static final String ACTIVITY_NAME = "OCTranspoActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_octranspo);
+    }
+
+    @Override
+    protected void onResume() {
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.i(ACTIVITY_NAME, "In onStart()");
+        super.onStart();
+    }
+
+
+    @Override
+    protected void onPause() {
+        Log.i(ACTIVITY_NAME, "In onPause()");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.i(ACTIVITY_NAME, "In onStop()");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+        super.onDestroy();
     }
 }
