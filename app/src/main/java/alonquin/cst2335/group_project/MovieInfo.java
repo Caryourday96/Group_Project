b55209707a947e43c2c29f2f7e1c85198cf249d7
Caryourday <adetkay@gmail.com>
2018-03-30 13:16:47 -0400

    Kayode's First Branch
    Signed-off-by: Caryourday <adetkay@gmail.com>

--- app/src/main/java/alonquin/cst2335/group_project/MovieInfo.java
+++ app/src/main/java/alonquin/cst2335/group_project/MovieInfo.java
@@ -1,7 +1,7 @@
 package alonquin.cst2335.group_project;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MovieInfo extends AppCompatActivity {
 
@@ -10,4 +10,6 @@ public class MovieInfo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_info);
     }
+
+    
 }
